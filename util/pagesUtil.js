0d6dee5c0c1ec0a22e4c7483e478a52d6d771e7d
lucasmodin <lumo0002@stud.kea.dk>
2025-10-18 00:53:37 +0200

    express page SSR serving done

--- util/pagesUtil.js
+++ util/pagesUtil.js
@@ -3,6 +3,7 @@ import { readPage, constructPage } from './templatingEngine.js';
 const frontPage = readPage("./public/pages/frontend/index.html");
 const gitPage = readPage("./public/pages/gitPage/git.html");
 const javascriptPage = readPage("./public/pages/javascriptPage/javascriptPage.html");
+const expressPage = readPage("./public/pages/expressPage/expressPage.html");
 
 export const frontendPage = constructPage(frontPage, {
     tabTitle: "Portfolio | Welcome",
@@ -41,5 +42,18 @@ export const javascriptFrontendPage = constructPage(javascriptPage, {
     scriptLinks: `
         <script src="/assets/js/footer.js"></script>
     `
-})
+});
+
+export const expressFrontendPage = constructPage(expressPage, {
+    tabTitle: "Express.js",
+    cssLinks: `
+        <link rel="stylesheet" href="/assets/css/main.css">
+        <link rel="stylesheet" href="/assets/css/header.css">
+        <link rel="stylesheet" href="/assets/css/footer.css">
+        <link rel="stylesheet" href="/pages/expressPage/expressPage.css">
+    `,
+    scriptLinks: `
+        <script src="/assets/js/footer.js"></script>
+    `    
+});
 
